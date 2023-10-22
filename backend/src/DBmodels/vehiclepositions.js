eafcb83b10e4651ba6f1d7170a47f4e6fef79075
PhamNhuNgoc <100960531+PhamNhuNgoc@users.noreply.github.com>
2023-10-22 17:10:35 +0700

    import Vehicle Positions (not finished)

---
+++ backend/src/DBmodels/vehiclepositions.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const vehiclePositionsSchema = new Schema({
+    trip_id: String,
+    route: { type: Schema.Types.ObjectId, ref: 'Route' }
+    timestamp: Date,
+    position: {
+            latitude: Number,
+            longitude: Number
+    },
+    speed: Number,
+    current_stop_sequence: Number,
+    current_status: Number,
+    stop_id: String,
+})
+
+const VehiclePositions = mongoose.model('VehiclePositions', vehiclePositionsSchema);
+
+export { VehiclePositions };
