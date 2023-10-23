aa0a03cb70b391273bc680c50d4843d7a866efb9
SiRi0000 <siriporn.senma@gmail.com>
2023-10-23 14:57:12 +0700

    changes to the realtime files

--- backend/src/gtfs-realtime/VehiclePositions.js
+++ backend/src/gtfs-realtime/VehiclePositions.js
@@ -8,7 +8,8 @@ const requestSettings = {
 }; 
 request(requestSettings, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    const feed = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(body);
+
+    const feed = GtfsRealtimeBindings.default.transit_realtime.FeedMessage.decode(body);
     feed.entity.forEach(function(entity) {
       // if (entity.trip_update) {
       //   console.log(entity.trip_update);
---
+++ backend/src/util.js
@@ -0,0 +1,3 @@
+const timestampInSeconds = 1698027739;
+const date = new Date(timestampInSeconds * 1000);
+console.log(date.toLocaleDateString());
