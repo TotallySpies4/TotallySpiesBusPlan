b86b3d2554c7ae3a91db28cc0ff1284fee8f1f63
linh <khanhlinhvng>
2023-10-23 15:16:40 +0700

    congestion level but need to check again the parameters of 2 called functions

---
+++ backend/src/congestionLevel.js
@@ -0,0 +1,19 @@
+import { calculateSpeedForRoute } from "./speedHelper";
+import { calculateRouteAvgSpeed } from "./RouteAvgSpeed";
+// import mongoose from "mongoose";
+
+export async function congestionLevel(){
+    const scheduleSpeed = calculateSpeedForRoute(routeID);
+    const route_avg_speed = calculateRouteAvgSpeed(vehiclePosition);
+    if(vehiclePosition.tripID===routeID.tripI && vehiclePosition.stop_sequence===routeID.stop_sequence){
+        if(scheduleSpeed<route_avg_speed){
+            if(scheduleSpeed<route_avg_speed+10){
+                return 1; // yellow
+            }else{
+                return 2; // red
+            }
+        }else{
+            return 0; // green
+        }
+    }
+}
