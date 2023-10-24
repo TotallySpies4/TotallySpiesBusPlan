32fedb26f155ac2d6a50d637ee1a7a4e6b9d3c3f
SiRi0000 <siriporn.senma@gmail.com>
2023-10-24 17:44:55 +0700

    update producer

--- backend/src/gtfs-realtime/realtimeProducer.js
+++ backend/src/gtfs-realtime/realtimeProducer.js
@@ -14,14 +14,62 @@ const producer = kafka.producer();
 
 
 
+const MAX_MESSAGE_SIZE = 1000000; // Limit the message size to 1MB
+
 async function sendToKafka(data) {
     await producer.connect();
-    await producer.send({
-        topic,
-        messages: [{ value: JSON.stringify(data) }],
-    });
+
+    const jsonString = JSON.stringify(data);
+    console.log("Sending data to Kafka");
+    console.log("Data size",Buffer.from(jsonString).length)
+    if (Buffer.from(jsonString).length > MAX_MESSAGE_SIZE) {
+        // If the message is too big, split it into chunks
+        const chunks = splitDataIntoChunks(data, MAX_MESSAGE_SIZE);
+
+        for (const chunk of chunks) {
+            console.log(`Sending chunk of size ${Buffer.from(JSON.stringify(chunk)).length} to Kafka`);
+            await producer.send({
+                topic,
+                messages: [{ value: JSON.stringify(chunk) }],
+            });
+        }
+    } else {
+        // If the message is small enough, send it as is
+        await producer.send({
+            topic,
+            messages: [{ value: jsonString }],
+        });
+    }
+
+    console.log("Data sent to Kafka");
     await producer.disconnect();
 }
+
+function splitDataIntoChunks(data, maxSize) {
+    const chunks = [];
+    let chunk = [];
+
+    let currentSize = 0;
+    for (const item of data) {
+        const itemSize = Buffer.from(JSON.stringify(item)).length;
+
+        if (currentSize + itemSize > maxSize) {
+            chunks.push(chunk);
+            chunk = [];
+            currentSize = 0;
+        }
+
+        chunk.push(item);
+        currentSize += itemSize;
+    }
+
+    if (chunk.length > 0) {
+        chunks.push(chunk);
+    }
+
+    return chunks;
+}
+
 
 async function clearAndRecreateTopic() {
     const admin = kafka.admin();
@@ -59,6 +107,7 @@ function fetchAndSend() {
         }
 
         const feed = GtfsRealtimeBindings.default.transit_realtime.FeedMessage.decode(body);
+
 
         console.log(feed.entity);
         await clearAndRecreateTopic();
