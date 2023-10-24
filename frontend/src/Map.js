0a6b11f986eb1ec7f750962f15b870d05ec479e2
Phạm Nguyễn Đan Quỳnh <quyndanphamnguyen@gmail.com>
2023-10-24 23:29:19 +0700

    First congestion display demo

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,16 +1,22 @@
-import { MapContainer, Marker, Popup, Polyline, TileLayer } from "react-leaflet";
-import L from 'leaflet';
-import 'leaflet-polylinedecorator';
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  Polyline,
+  TileLayer,
+} from "react-leaflet";
+import L from "leaflet";
+import "leaflet-polylinedecorator";
 import PolylineDecorator from "./PolylineDecorator.js";
-import {useEffect} from "react";
+import { useEffect, useState } from "react";
 
 const Map = ({ selectedBus, setSidebarOpen, isSidebarOpen }) => {
+  const [congestionLevel, setCongestionLevel] = useState(0); // Set the initial congestion level to 0
 
-        //console.log("selected bus in MAP: "+selectedBus)
-    useEffect(() => {
-        console.log("selected bus in MAP: "+selectedBus)
-    }, [selectedBus]);
-
+  //console.log("selected bus in MAP: "+selectedBus)
+  useEffect(() => {
+    console.log("selected bus in MAP: " + selectedBus);
+  }, [selectedBus]);
 
   return (
     <div className="map">
@@ -32,42 +38,53 @@ const Map = ({ selectedBus, setSidebarOpen, isSidebarOpen }) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-        {selectedBus ? selectedBus.stop_times.map((stopTime) => {
-            return (
-            <Marker
-              key={stopTime.stop_id}
-              position={
-                [
-                stopTime.location.latitude,
-                stopTime.location.longitude,
-                ]
-            }>
-              <Popup>
-                {stopTime.stop_name} - {stopTime.arrival_time.slice(0, -3)}
-              </Popup>
-            </Marker>
+        {selectedBus
+          ? selectedBus.stop_times.map((stopTime) => {
+              return (
+                <Marker
+                  key={stopTime.stop_id}
+                  position={[
+                    stopTime.location.latitude,
+                    stopTime.location.longitude,
+                  ]}>
+                  <Popup>
+                    {stopTime.stop_name} - {stopTime.arrival_time.slice(0, -3)}
+                  </Popup>
+                </Marker>
+              );
+            })
+          : null}
+
+        {selectedBus && selectedBus.routeCoordinates && (
+          <>
+            <Polyline
+              positions={selectedBus.routeCoordinates.map((stopTime) => [
+                stopTime.shape_pt_lat,
+                stopTime.shape_pt_lon,
+              ])}
+              color="blue"
+            />
+            <PolylineDecorator
+              positions={selectedBus.routeCoordinates.map((stopTime) => [
+                stopTime.shape_pt_lat,
+                stopTime.shape_pt_lon,
+              ])}
+              patterns={[
+                {
+                  offset: "10%",
+                  repeat: "20%",
+                  symbol: L.Symbol.arrowHead({
+                    pixelSize: 10,
+                    pathOptions: { fillOpacity: 1, weight: 0 },
+                  }),
+                },
+              ]}
+              congestionLevel={congestionLevel} // Pass the congestionLevel prop
+            />
+          </>
         )}
-          ): null}
-
-            {selectedBus && selectedBus.routeCoordinates && (
-                <>
-                    <Polyline
-                        positions={selectedBus.routeCoordinates.map(stopTime => [stopTime.shape_pt_lat, stopTime.shape_pt_lon])}
-                        color='blue'
-                    />
-                    <PolylineDecorator
-                        positions={selectedBus.routeCoordinates.map(stopTime => [stopTime.shape_pt_lat, stopTime.shape_pt_lon])}
-                        patterns={[
-                            { offset: '10%', repeat: '20%', symbol: L.Symbol.arrowHead({ pixelSize: 10, pathOptions: { fillOpacity: 1, weight: 0 } }) }
-                        ]}
-                    />
-                </>
-
-            )}
-        </MapContainer>
-
-      </div>
-
+      </MapContainer>
+    </div>
   );
 };
 
--- frontend/src/PolylineDecorator.js
+++ frontend/src/PolylineDecorator.js
@@ -1,11 +1,45 @@
+import React, { useEffect, useRef } from 'react';
 import L from 'leaflet';
+import 'leaflet-polylinedecorator';
 
-const PolylineDecorator =({positions,patterns}) => {
+const PolylineDecorator = ({ positions, patterns, congestionLevel }) => {
+  const mapRef = useRef(null); 
+  
+  useEffect(() => {
+    // Get the Leaflet map instance from the ref
+    const map = mapRef.current;
 
-    return (
-        <div>
+    // Create a Leaflet Polyline layer
+    const polyline = L.polyline(positions);
 
-        </div>
-    )
-}
-export default PolylineDecorator
+    // Create a Leaflet PolylineDecorator layer
+    const decorator = L.polylineDecorator(polyline, { patterns });
+
+    // Add the PolylineDecorator layer to the map
+    decorator.addTo(map);
+
+    // Update the polyline color based on the congestion level
+    const updatePolylineColor = () => {
+      const congestionColors = {
+        0: 'bg-green-500',
+        1: 'bg-yellow-500',
+        2: 'bg-red-500',
+      };
+      const colorClass = congestionColors[congestionLevel];
+      polyline.setStyle({ color: 'transparent' });
+      polyline.setStyle({ fillColor: 'transparent' });
+      polyline.setStyle({ className: colorClass });
+    };
+
+    updatePolylineColor();
+
+    return () => {
+      
+      decorator.removeFrom(map);
+    };
+  }, [positions, patterns, congestionLevel]);
+
+  return <div ref={mapRef} style={{ display: 'none' }}></div>;
+};
+
+export default PolylineDecorator;
