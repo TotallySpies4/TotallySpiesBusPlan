e70e6d7c309e1a8ca670a8d11d7ff22d4cc341a0
SiRi0000 <siriporn.senma@gmail.com>
2023-11-14 22:46:34 +0700

    got sl.zip

--- backend/src/testLocation.js
+++ backend/src/testLocation.js
@@ -1,28 +1,24 @@
 import request from "request";
-import * as GtfsRealtimeBindings from "gtfs-realtime-bindings";
 import * as fs from "fs";
+
 const requestSettings = {
     method: 'GET',
-    url:'https://api.koda.trafiklab.se/KoDa/api/v2/gtfs-rt/klt/VehiclePositions?date=2021-01-01&key=gQpNjugJMEZZKu69pw3Sbz4PrLhZ0K_hVDGH5RAGUqk'
-
-
-
+    url: 'https://api.koda.trafiklab.se/KoDa/api/v2/gtfs-rt/klt/VehiclePositions?date=2021-01-01&key=gQpNjugJMEZZKu69pw3Sbz4PrLhZ0K_hVDGH5RAGUqk',
+    encoding: null // This ensures that the response is treated as binary data
 };
-let outPutPath = "history.7z"
-request(requestSettings, async function (error, response, body) {
-    console.log("test2");
 
+let outPutPath = "history.7z";
+
+request(requestSettings, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-        const responseBody = Buffer.from(body, 'binary');
-        console.log(response.statusCode);
-        fs.writeFile(outPutPath, responseBody, function (err) {
+        fs.writeFile(outPutPath, body, function (err) {
             if (err) {
-                console.log(err);
+                console.error(err);
+            } else {
+                console.log("The file was saved!");
             }
-            console.log("The file was saved!");
-        })
-
-        //const feed = GtfsRealtimeBindings.default.transit_realtime.FeedMessage.decode(body);
-
+        });
+    } else {
+        console.error("Error while downloading file:", error);
     }
 });
