916d59d0f7aeb55ad7403794e0f572deea408585
SiRi0000 <siriporn.senma@gmail.com>
2023-11-15 13:06:45 +0700

    add clearDB.js script

---
+++ backend/src/clearDB.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://mongodb:27017/TotallySpiesBusPlan', {
+    serverSelectionTimeoutMS: 60000
+})
+    .then(() => {
+        console.log('Connected to MongoDB')
+        mongoose.connection.db.dropDatabase()})
+    .catch(err => console.error('Could not connect to MongoDB:', err));
