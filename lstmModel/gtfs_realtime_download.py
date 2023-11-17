944ce033eec5da74fc20bc1753682f15b11ad641
PhamNhuNgoc <100960531+PhamNhuNgoc@users.noreply.github.com>
2023-11-17 22:03:11 +0700

    Store gtfs realtime data

--- lstmModel/gtfs_realtime_download.py
+++ lstmModel/gtfs_realtime_download.py
@@ -1,11 +1,7 @@
 import os
-import json
 import requests
 import py7zr
 from google.transit import gtfs_realtime_pb2
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode
-
 
 # Define the API endpoint
 api_url = "https://api.koda.trafiklab.se/KoDa/api/v2/gtfs-rt/klt/VehiclePositions?date=2021-01-01&key=gQpNjugJMEZZKu69pw3Sbz4PrLhZ0K_hVDGH5RAGUqk"
@@ -31,24 +27,6 @@ if response.status_code == 200:
         with py7zr.SevenZipFile("history.7z", mode="r") as archive:
             archive.extractall(output_directory)
         print("7z file extracted successfully.")
-
-        # Initialize Spark session
-        spark = SparkSession.builder \
-            .appName("GTFSRealtimeAnalysis") \
-            .config("spark.jars", "./jars/spark-protobuf_2.11-1.0.0.jar") \
-            .getOrCreate()
-
-        # Read pb files into a DataFrame using spark-protobuf library
-        df = spark.read.format("protobuf").option("inputFormat", "protobuf").load(output_directory)
-
-        # Assuming the vehicle position data is nested in a column named 'positions'
-        exploded_df = df.select("other_columns", explode("positions").alias("position"))
-
-        # Write the original DataFrame to a CSV file
-        exploded_df.write.csv("consolidated_data.csv", header=True)
-
-        # Stop the Spark session
-        spark.stop()
 
     except py7zr.exceptions.Bad7zFile as e:
         print(f"Error extracting 7z file: {e}")
