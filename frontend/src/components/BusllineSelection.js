ff4789f513212636ae435c6f29ad6c07a4d89a05
Phạm Nguyễn Đan Quỳnh <quyndanphamnguyen@gmail.com>
2023-11-20 21:52:47 +0700

    First Proto Of Busline Selection and Station Info

--- frontend/src/components/BusllineSelection.js
+++ frontend/src/components/BusllineSelection.js
@@ -1,55 +1,88 @@
+import React, { useState } from "react";
 import Select from "react-select";
-import {formatTime} from "../utils/utils.js";
+import { formatTime } from "../utils/utils";
+import SingleStationInfo from "./SingleStationInfo";
 
-export const BusllineSelection = ({selectedCity,selectedTrip,setSelectedBusline,allroutes}) => {
-    let busOptions;
+const BuslineSelection = ({
+  selectedCity,
+  setSelectedBusline,
+  allroutes,
+  congestionStatus,
+  currentVehicle,
+}) => {
+  let busOptions;
 
+  const customStyles = {
+    control: (provided, state) => ({
+      ...provided,
+      borderColor: "#3b82f6",
+      borderWidth: "2px",
+      ":hover": {
+        borderColor: "#3b82f6",
+      },
+    }),
+    singleValue: (provided, state) => ({
+      ...provided,
+      color: state.selectProps.value === null ? "grey" : "#3b82f6",
+    }),
 
-        if(selectedCity === "Amsterdam") {
-            busOptions = allroutes.amsterdam.map((bus) => ({
-                value: bus.route_short_name,
-                label: bus.route_short_name + " - " + bus.route_long_name,
-            }));
-        } else if (selectedCity === "Stockholm") {
-            busOptions = allroutes.stockholm.map((bus) => ({
-                value: bus.route_short_name,
-                label: bus.route_short_name + " - " + bus.route_long_name,
-            }));
-        }
+    dropdownIndicator: (provided, state) => ({
+      ...provided,
+      color: "#3b82f6",
+      ":hover": {
+        color: "#3b82f6",
+      },
+    }),
+  };
 
-    const handleBusSelection = (option) => {
-        let bus;
-        if(selectedCity === "Amsterdam") {
-            bus = allroutes.amsterdam.find((b) => b.route_short_name === option.value);
-        } else if (selectedCity === "Stockholm") {
-            bus = allroutes.stockholm.find((b) => b.route_short_name === option.value);
-        }
-        console.log("SelectedBusID: " + bus.route_id);
-        setSelectedBusline(bus);
-    };
-    return (
+  if (selectedCity === "Amsterdam") {
+    busOptions = allroutes.amsterdam.map((bus) => ({
+      value: bus.route_short_name,
+      label: (
         <div>
-            <Select
-                options={busOptions}
-                onChange={handleBusSelection}
-                isSearchable
-                placeholder="Choose a bus line..."
-                className="w-full"
-            />
-
-            {selectedTrip && selectedTrip.stop_times && (
-                <div className="list">
-
-                    <ul>
-                        {selectedTrip.stop_times.map((stop, index) => (
-                            <li key={index}>
-                                {stop.stop_name} - {formatTime(stop.arrival_time)}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+          <strong>{bus.route_short_name}</strong> {bus.route_long_name}
         </div>
+      ),
+    }));
+  } else if (selectedCity === "Stockholm") {
+    busOptions = allroutes.stockholm.map((bus) => ({
+      value: bus.route_short_name,
+      label: (
+        <div>
+          <strong>{bus.route_short_name}</strong> {bus.route_long_name}
+        </div>
+      ),
+    }));
+  }
 
-    );
-}
+  const handleBusSelection = (option) => {
+    let bus;
+    if (selectedCity === "Amsterdam") {
+      bus = allroutes.amsterdam.find(
+        (b) => b.route_short_name === option.value
+      );
+    } else if (selectedCity === "Stockholm") {
+      bus = allroutes.stockholm.find(
+        (b) => b.route_short_name === option.value
+      );
+    }
+    setSelectedBusline(bus);
+  };
+
+  return (
+    <div>
+      <Select
+        options={busOptions}
+        onChange={handleBusSelection}
+        isSearchable
+        placeholder="-Choose a bus line-"
+        className="w-full"
+        styles={customStyles}
+      />
+
+      {selectedCity && <SingleStationInfo />}
+    </div>
+  );
+};
+
+export default BuslineSelection;
---
+++ frontend/src/components/SingleStationInfo.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { formatTime } from "../utils/utils";
+
+const SingleStationInfo = ({
+  selectedTrip,
+  congestionStatus,
+  currentVehicle,
+}) => {
+  return (
+    selectedTrip &&
+    selectedTrip.stop_times && (
+      <div className="list" style={{ maxHeight: "300px", overflowY: "auto" }}>
+        <ul>
+          {selectedTrip.stop_times.map((stop, index) => (
+            <li key={index}>
+              <div className="flex flex-row justify-between items-center divide-y-2">
+                <div>
+                  <strong>{stop.stop_name}</strong>
+                  {congestionStatus && (
+                    <div
+                      style={{
+                        color: getCongestionInfo(
+                          currentVehicle.congestion_level.level
+                        ).color,
+                      }}>
+                      {
+                        getCongestionInfo(currentVehicle.congestion_level.level)
+                          .status
+                      }
+                    </div>
+                  )}
+                </div>
+                <strong
+                  style={{
+                    color: getCongestionInfo(
+                      currentVehicle.congestion_level.level
+                    ).color,
+                  }}>
+                  {formatTime(stop.arrival_time)}
+                </strong>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  );
+};
+
+function getCongestionInfo(level) {
+  switch (level) {
+    case 1:
+      return { status: "On time", color: "green" };
+    case 2:
+      return { status: "Maybe late", color: "orange" };
+    case 3:
+      return { status: "Late", color: "red" };
+    default:
+      return { status: "Bus is not in operation", color: "grey" };
+  }
+}
+
+export default SingleStationInfo;
