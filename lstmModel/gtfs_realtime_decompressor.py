5ec71ce739d17bb3c05bf8c77b10c509132b6dd7
SiRi0000 <siriporn.senma@gmail.com>
2023-11-20 14:40:45 +0700

    create a data processing pipeline (decompress, process, save csv to db)

--- lstmModel/gtfs_realtime_decompressor.py
+++ lstmModel/gtfs_realtime_decompressor.py
@@ -4,11 +4,12 @@ import logging
 from py7zr import py7zr
 
 
-def decompress_7z_file(zip_file):
+def decompress_7z_file(zip_file_path):
     try:
-        with py7zr.SevenZipFile(zip_file, mode="r") as archive:
-            archive.extractall()
+        with py7zr.SevenZipFile(zip_file_path, mode="r") as archive:
+            directory = os.path.dirname(zip_file_path)
+            archive.extractall(path=directory)
+            return os.path.join(directory, next(iter(archive.getnames()), None))
     except py7zr.exceptions.Bad7zFile as e:
-        logging.info(f"Fehler beim Extrahieren der 7z-Datei: {e}")
-
-
+        logging.error(f"Fehler beim Extrahieren der 7z-Datei: {e}")
+        return None
