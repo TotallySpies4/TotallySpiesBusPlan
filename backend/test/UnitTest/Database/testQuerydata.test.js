b1bf72e4d8fd755a3577eb150178c97a721bf3d1
PhamNhuNgoc <100960531+PhamNhuNgoc@users.noreply.github.com>
2023-12-16 02:45:11 +0700

    Update testQuerydata.test.js

--- backend/test/UnitTest/Database/testQuerydata.test.js
+++ backend/test/UnitTest/Database/testQuerydata.test.js
@@ -5,168 +5,209 @@ import {
   fetchAverageSpeed,
 } from "../../../src/queryData/queryDbData.js";
 import { Route, StopTime, Trip } from "../../../src/DBmodels/busline.js";
-import { calculatorScheduledSpeedAmsterdam } from "../../../src/utils/speedCalculator.js";
+import { TripUpdate } from "../../../src/DBmodels/tripUpdate.js";
+import { SegmentSpeedPrediction } from "../../../src/DBmodels/segmentSpeedPrediction.js";
 import { VehiclePositions } from "../../../src/DBmodels/vehiclepositions.js";
+import { calculatorScheduledSpeedAmsterdam, calculatorScheduledSpeedStockholm } from "../../../src/utils/speedCalculator.js";
 import { getShapesBetweenStops } from "../../../src/utils/shapesUtilSet.js";
 import { agency } from "../../../src/utils/enum.js";
+import { populate } from 'mongoose';
 
-jest.mock('../../../src/DBmodels/busline.js', () => ({
-  Route: {
-    find: jest.fn(),
-    findOne: jest.fn().mockResolvedValue({ _id: 'someRouteObjID', trips: ['someTripID'] }),
-  },
-  Trip: {
-    findOne: jest.fn(),
-    populate: jest.fn(),
-  },
-  StopTime: {
-    findOne: jest.fn(),
-    populate: jest.fn(),
-  },
-}));
+// Mock the necessary dependencies
+jest.mock("../../../src/DBmodels/busline.js");
+jest.mock("../../../src/DBmodels/vehiclepositions.js");
+jest.mock("../../../src/utils/speedCalculator.js");
+jest.mock("../../../src/utils/shapesUtilSet.js");
+jest.mock("../../../src/DBmodels/tripUpdate.js");
+jest.mock("../../../src/DBmodels/segmentSpeedPrediction.js");
 
-jest.mock('../../../src/DBmodels/vehiclepositions.js', () => ({
-  VehiclePositions: {
-    findOne: jest.fn(),
-  },
-}));
+describe('Busline Service', () => {
+  describe('getBusAllBuslineAmsterdam', () => {
+    it('should retrieve all bus lines from Amsterdam', async () => {
+      // Mock the Route.find() method to return a dummy response
+      Route.find.mockResolvedValue(['busLine1', 'busLine2']);
 
-jest.mock('../../../src/utils/shapesUtilSet.js', () => ({
-  getShapesBetweenStops: jest.fn(),
-}));
+      // Call the function being tested
+      const result = await getBusAllBuslineAmsterdam();
 
-jest.mock('../../../src/utils/speedCalculator.js', () => ({
-  calculatorScheduledSpeedAmsterdam: jest.fn(),
-}));
+      // Check the expected result
+      expect(result).toEqual(['busLine1', 'busLine2']);
 
-describe('getBusAllBuslineAmsterdam', () => {
-  it('should get all bus lines from Amsterdam', async () => {
-    // Mock the behavior of the Route model
-    Route.find.mockResolvedValueOnce([{ name: 'GVB' }]);
-
-    const result = await getBusAllBuslineAmsterdam();
-
-    // Assertions
-    expect(Route.find).toHaveBeenCalledWith({ agency_id: agency.GVB });
-    expect(result).toEqual([{ name: 'GVB' }]);
-  });
-});
-
-describe('getBusAllBuslineStockholm', () => {
-  it('should get all bus lines from Stockholm', async () => {
-    // Mock the behavior of the Route model
-    Route.find.mockResolvedValueOnce([{ name: 'SL' }]);
-
-    const result = await getBusAllBuslineStockholm();
-
-    // Assertions
-    expect(Route.find).toHaveBeenCalledWith({ agency_id: agency.SL });
-    expect(result).toEqual([{ name: 'SL' }]);
-  });
-});
-
-describe('getBusDetails', () => {
-  it('should get bus details', async () => {
-    // Mock data
-    const routeID = 'someRouteID';
-    const route = {
-      _id: 'someRouteObjID',
-      trips: ['someTripID'],
-    };
-    const currentVehicle = {
-      currentTrip_id: 'someTripID',
-      congestion_level: {
-        previousStop: 'previousStopID',
-        currentStop: 'currentStopID',
-      },
-    };
-    const trip = {
-      _id: 'someTripID',
-      stop_times: ['someStopTimeID'],
-      shapes: ['someShapeID'],
-    };
-    const previousStopTime = 'previousStopID';
-    const currentStopTime = 'currentStopID';
-    const congestionShape = ['someShapeID'];
-
-    // Mock the behavior of the Route model
-    Route.findOne.mockResolvedValueOnce(route);
-
-    // Mock the behavior of the VehiclePositions model
-    VehiclePositions.findOne.mockResolvedValueOnce(currentVehicle);
-
-    // Mock the behavior of the Trip model
-    Trip.findOne.mockResolvedValueOnce(trip);
-
-    // Mock the behavior of the getShapesBetweenStops function
-    getShapesBetweenStops.mockResolvedValueOnce(congestionShape);
-
-    // Call the getBusDetails function
-    const result = await getBusDetails(routeID);
-
-    // Assertions
-    expect(Route.findOne).toHaveBeenCalledWith({ route_id: routeID });
-    expect(VehiclePositions.findOne).toHaveBeenCalledWith({
-      route: route._id,
-    });
-    expect(Trip.findOne).toHaveBeenCalledWith({
-      _id: currentVehicle.currentTrip_id,
-    });
-    expect(getShapesBetweenStops).toHaveBeenCalledWith(
-      trip.shapes,
-      previousStopTime,
-      currentStopTime
-    );
-    expect(result).toEqual({
-      currentVehicle,
-      trip,
-      congestionShape,
+      // Verify that the Route.find() method was called
+      expect(Route.find).toHaveBeenCalledWith({ agency_id: agency.GVB });
     });
   });
-});
 
-describe('fetchAverageSpeed', () => {
-  it('should fetch the average speed', async () => {
-    // Mock data
-    const routeID = 'someRouteID';
-    const tripID = 'someTripID';
-    const stopSequence = 42;
-    const speedEntry = 30;
-    const previousStop = 'previousStopID';
-    const currentStop = 'currentStopID';
+  describe('getBusAllBuslineStockholm', () => {
+    it('should retrieve all bus lines from Stockholm', async () => {
+      // Mock the Route.find() method to return a dummy response
+      Route.find.mockResolvedValue(['busLine1', 'busLine2']);
 
-    // Mock the behavior of the StopTime model
-    StopTime.findOne.mockResolvedValueOnce({
-      shape_dist_traveled: 10,
-    });
-    StopTime.findOne.mockResolvedValueOnce({
-      shape_dist_traveled: 20,
-    });
+      // Call the function being tested
+      const result = await getBusAllBuslineStockholm();
 
-    // Mock the behavior of the calculatorScheduledSpeedAmsterdam function
-    calculatorScheduledSpeedAmsterdam.mockResolvedValueOnce(speedEntry);
+      // Check the expected result
+      expect(result).toEqual(['busLine1', 'busLine2']);
 
-    // Call the fetchAverageSpeed function
-    const result = await fetchAverageSpeed(routeID, tripID, stopSequence);
-
-    // Assertions
-    expect(StopTime.findOne).toHaveBeenCalledWith({
-      trip_id: tripID,
-      stop_sequence: stopSequence - 1,
-    });
-    expect(StopTime.findOne).toHaveBeenCalledWith({
-      trip_id: tripID,
-      stop_sequence: stopSequence,
-    });
-    expect(calculatorScheduledSpeedAmsterdam).toHaveBeenCalledWith(
-      routeID,
-      10,
-      20
-    );
-    expect(result).toEqual({
-      speedEntry,
-      previousStop,
-      currentStop,
+      // Verify that the Route.find() method was called
+      expect(Route.find).toHaveBeenCalledWith({ agency_id: agency.SL });
     });
   });
-});
+
+  describe('getBusDetails', () => {
+      it('should retrieve bus details for a given routeID', async () => {
+        // Mock the necessary dependencies and their responses
+        const route = {
+          _id: 'routeId',
+          trips: ['tripId1', 'tripId2'],
+        };
+        const currentVehicle = {
+          currentTrip_id: 'currentTripId',
+          congestion_level: {
+            previousStop: 'previousStop',
+            currentStop: 'currentStop',
+          },
+        };
+        const trip = {
+          _id: 'tripId',
+          shapes: ['shape1', 'shape2'],
+        };
+        const tripUpdate = {
+          stopTimeUpdates: ['update1', 'update2'],
+        };
+        const segmentSpeedPrediction = ['prediction1', 'prediction2'];
+
+        // Mock the necessary methods from the models
+        const populate = jest.fn().mockResolvedValueOnce(currentVehicle);
+        Route.findOne.mockReturnValue({ populate: populate.mockReturnValueOnce({}) });
+        VehiclePositions.findOne.mockResolvedValue(currentVehicle);
+        Trip.findOne.mockResolvedValue({ toObject: jest.fn().mockReturnValue(trip) });
+        TripUpdate.findOne.mockResolvedValue(tripUpdate);
+        SegmentSpeedPrediction.find.mockResolvedValue(segmentSpeedPrediction);
+
+        // Call the function being tested
+        const result = await getBusDetails('routeId');
+
+        // Check the expected result
+        expect(result).toEqual({
+          currentVehicle,
+          trip,
+          congestionShape: ['shape1', 'shape2'],
+          updateStoptime: tripUpdate.stopTimeUpdates,
+          segmentSpeedPrediction,
+        });
+
+        // Verify that the necessary methods were called
+        expect(Route.findOne).toHaveBeenCalledWith({ route_id: 'routeId' });
+        expect(VehiclePositions.findOne).toHaveBeenCalledWith({ route: 'routeId' });
+        expect(Trip.findOne).toHaveBeenCalledWith({ _id: 'currentTripId' });
+        expect(TripUpdate.findOne).toHaveBeenCalledWith({ trip_id: 'tripId' });
+        expect(SegmentSpeedPrediction.find).toHaveBeenCalledWith({ trip_id: 'tripId' });
+        expect(populate).toHaveBeenCalledWith('currentTrip_id', 'congestion_level.previousStop', 'congestion_level.currentStop');
+      });
+    });
+
+
+  describe('fetchAverageSpeed', () => {
+    it('should fetch the average speed for a given route and direction', async () => {
+      // Mock the necessary dependencies and their responses
+      const routeId = 'routeId';
+      const direction = 'direction';
+      const startTime = new Date('2023-12-15T10:00:00Z');
+      const endTime = new Date('2023-12-15T11:00:00Z');
+      const segmentSpeedPredictions = [
+        { speed: 30 },
+        { speed: 40 },
+        { speed: 35 },
+        { speed: 45 },
+      ];
+
+      // Mock the necessary methods from the models
+      SegmentSpeedPrediction.find.mockResolvedValue(segmentSpeedPredictions);
+
+      // Call the function being tested
+      const result = await fetchAverageSpeed(routeId, direction, startTime, endTime);
+
+      // Calculate the expected average speed
+      const totalSpeed = segmentSpeedPredictions.reduce((sum, prediction) => sum + prediction.speed, 0);
+      const averageSpeed = totalSpeed / segmentSpeedPredictions.length;
+
+      // Check the expected result
+      expect(result).toBe(averageSpeed);
+
+      // Verify that the necessary methods were called
+      expect(SegmentSpeedPrediction.find).toHaveBeenCalledWith({
+        route_id: routeId,
+        direction,
+        startTime: { $gte: startTime },
+        endTime: { $lte: endTime },
+      });
+    });
+
+    it('should fetch the average speed for a given routeID, tripID, and stop sequence (Stockholm)', async () => {
+      // Mock the necessary dependencies and their responses
+      const route = {
+        trips: [
+          { trip_id: 'tripId1', stop_times: ['stopTimeId11', 'stopTimeId12'] },
+          { trip_id: 'tripId2', stop_times: ['stopTimeId21', 'stopTimeId22'] },
+        ],
+      };
+      const currentStopTime = {
+        _id: 'stopTimeId12',
+        arrival_time: '12:00:00',
+        departure_time: '12:05:00',
+      };
+      const previousStopTime = {
+        _id: 'stopTimeId11',
+        arrival_time: '11:50:00',
+        departure_time: '11:55:00',
+      };
+      const speed = 50;
+
+      Route.findOne.mockReturnValue({ populate: populate.mockReturnValueOnce({}) });
+      StopTime.findOne.mockResolvedValueOnce(currentStopTime).mockResolvedValueOnce(previousStopTime);
+      calculatorScheduledSpeedStockholm.mockReturnValue(speed);
+
+      // Call the function being tested
+      const result = await fetchAverageSpeed('routeId', 'tripId', 2);
+
+      // Check the expected result
+      expect(result).toEqual(speed);
+
+      // Verify that the necessary methods were called
+      expect(Route.findOne).toHaveBeenCalledWith({ route_id: 'routeId' });
+      expect(StopTime.findOne).toHaveBeenNthCalledWith(1, { trip_id: 'tripId', stop_sequence: 2 });
+      expect(StopTime.findOne).toHaveBeenNthCalledWith(2, { trip_id: 'tripId', stop_sequence: 1 });
+      expect(calculatorScheduledSpeedStockholm).toHaveBeenCalledWith(
+        currentStopTime.arrival_time,
+        currentStopTime.departure_time,
+        previousStopTime.arrival_time,
+        previousStopTime.departure_time
+      );
+    });
+  });
+
+  describe('getShapesBetweenStops', () => {
+      it('should retrieve the shapes between two stops', async () => {
+        // Mock the necessary dependencies and their responses
+        const route = {
+          _id: 'routeId',
+          trips: ['tripId1', 'tripId2'],
+          populate: jest.fn().mockResolvedValueOnce(route),
+        };
+        Route.findOne.mockResolvedValue(route);
+        Trip.findOne.mockResolvedValue({ toObject: jest.fn().mockReturnValue(route) });
+        getShapesBetweenStops.mockResolvedValue(['shape1', 'shape2']);
+
+        // Call the function being tested
+        const result = await getShapesBetweenStops(route, 'stopId1', 'stopId2');
+
+        // Check the expected result
+        expect(result).toEqual(['shape1', 'shape2']);
+
+        // Verify that the necessary methods were called
+        expect(Route.findOne).toHaveBeenCalledWith({ route_id: 'routeId' });
+        expect(getShapesBetweenStops).toHaveBeenCalledWith(route, 'stopId1', 'stopId2');
+      });
+    });
+  });
