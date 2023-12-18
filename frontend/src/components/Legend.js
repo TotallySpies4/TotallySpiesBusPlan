d2e4b7042d26f0f25dc3e18fc71d8b4c665e4a40
Phạm Nguyễn Đan Quỳnh <quyndanphamnguyen@gmail.com>
2023-12-18 22:01:37 +0700

    Update legend

--- frontend/src/components/Legend.js
+++ frontend/src/components/Legend.js
@@ -2,17 +2,17 @@ export const Legend = () => {
   return (
     <div className="legend-board absolute right-[5px] bottom-[10px] w-[160px] h-[126px]">
       <strong className="text-center">Congestion level</strong>
-      <div className="status normal">
-        <div className="line bg-[#88c36c]" />
-        <div className="title">Normal</div>
+      <div className="legend-status normal">
+        <div className="legend-line bg-[#88c36c]" />
+        <div className="legend-title">Normal</div>
       </div>
-      <div className="status low">
-        <div className="line bg-[#f5b873]" />
-        <div className="title">Low</div>
+      <div className="legend-status low">
+        <div className="legend-line bg-[#f5b873]" />
+        <div className="legend-title">Low</div>
       </div>
-      <div className="status high">
-        <div className="line bg-[#ff7070]" />
-        <div className="title">High</div>
+      <div className="legend-status high">
+        <div className="legend-line bg-[#ff7070]" />
+        <div className="legend-title">High</div>
       </div>
     </div>
   );
