69ac3dc13596b9e9470d9a2b17159a5faf4597f7
SiRi0000 <siriporn.senma@gmail.com>
2023-12-18 19:56:31 +0700

    python test

--- lstmModel/Test/UnitTest/Data_Prep/test_reader.py
+++ lstmModel/Test/UnitTest/Data_Prep/test_reader.py
@@ -1,38 +1,42 @@
+
 import unittest
-import os
-import py7zr
+from unittest.mock import patch, mock_open, MagicMock
 
-from src.Data_Prep.gtfs_realtime_decompressor import decompress_7z_file
+from google.transit import gtfs_realtime_pb2
 
-
-class TestDecompress7zFile(unittest.TestCase):
-
-    def setUp(self):
-        # Set up a valid 7z file for testing
-        self.valid_7z_file = '/Users/siri2001/IdeaProjects/TotallySpiesBusPlan/lstmModel/Test/UnitTest/Data_Prep/testStub/sample-1.7z'
-        # Create a valid 7z file or copy it to the test directory
-
-        # Set up a bad 7z file for testing
-        self.bad_7z_file = '/Users/siri2001/IdeaProjects/TotallySpiesBusPlan/lstmModel/Test/UnitTest/Data_Prep/testStub/sample-1-bad.7z'
-        # Create a bad 7z file or copy it to the test directory
-
-        # Non-existent file
-        self.nonexistent_file = 'file.7z'
-
-    def test_decompress_valid_file(self):
-        # Test decompression with a valid file
-        result = decompress_7z_file(self.valid_7z_file)
-        self.assertIsNotNone(result)
-        # Check if files are extracted
-        self.assertTrue(os.path.exists(result))
-
-    def test_decompress_bad_file(self):
-        # Test decompression with a bad file
-        result = decompress_7z_file(self.bad_7z_file)
-        self.assertIsNone(result)
+from src.Data_Prep.gtfs_realtime_reader import process_pb_directory
 
 
+class TestProcessPB(unittest.TestCase):
 
-    def tearDown(self):
-        # Clean up (remove extracted files, etc.) after tests
-        pass
+    @patch('src.Data_Prep.gtfs_realtime_reader.os.walk')
+    @patch('src.Data_Prep.gtfs_realtime_reader.open', new_callable=mock_open)
+    @patch('src.Data_Prep.gtfs_realtime_reader.csv.writer')
+    def test_process_pb_directory(self, mock_csv_writer, mock_open_file, mock_os_walk):
+        # Setup mock for os.walk to return a list of .pb files
+        mock_os_walk.return_value = [('/path/to/pb', [], ['file1.pb', 'file2.pb'])]
+
+        # Mock a pb file content
+        pb_content = b'your serialized pb content'
+        mock_open_file.return_value.read.return_value = pb_content
+
+        # Mock the FeedMessage object
+        mock_feed_message = MagicMock(spec=gtfs_realtime_pb2.FeedMessage)
+
+        # Setup the FeedMessage entities
+        mock_feed_message.entity = [
+            MagicMock(),  # You would set up this mock as needed
+        ]
+
+        # Mock the parsing of pb content
+        with patch('src.Data_Prep.gtfs_realtime_reader.gtfs_realtime_pb2.FeedMessage', return_value=mock_feed_message):
+            # Call the function to test
+            process_pb_directory('/path/to/pb', 'output.csv')
+
+            # Check that the CSV writer was created
+            mock_csv_writer.assert_called_once()
+
+            # Check that the pb files were read
+            mock_open_file().read.assert_called()
+
+            self.assertTrue(mock_csv_writer().writerow.called)
